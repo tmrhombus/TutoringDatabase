95c7b4f0d5e9265d9095e85348412c92727137da
Tom Perry <tomperry7@gmail.com>
2022-03-23 10:01:59 -0600

    database basic structure, puts correctly

--- Student.py
+++ Student.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-
+# class for datatype with multiple attributes
 class Student:
 
  # Constructor - student has a name
@@ -15,9 +15,61 @@ class Student:
  def changeaddress(self, theaddress):
   self.address = theaddress
 
+ # store it as a nested dictionary for database
  def storeinfo(self):
   info = { self.name : {"parents": self.parents, 
                         "address": self.address} 
          }
   return info
+
+
+import json
+import os
+
+# implementation of the actual database
+class StudentDB(object):
+
+ def __init__(self, location):
+  self.location = os.path.expanduser(location)
+  self.load(self.location)
+
+ # load the database if it exists
+ # create a new one if it doesn't
+ def load(self, location):
+  if os.path.exists(location):
+   print("DB exists, loading")
+   self._load()
+  else:
+   print("DB doesn't exist, creating a new one")
+   self.db = {}
+
+ # private function to actually load the database
+ def _load(self):
+  self.db = json.load(open(self.location, "r"))
+
+ # print the database in memory to file
+ def updatedb(self):
+  try:
+   json.dump(self.db, open(self.location, "w+"))
+  except:
+   print("There was a problem with writing the database")
+ 
+ # set a 
+ def set(self, student):
+  self.db.update(student)
+  self.updatedb()
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
