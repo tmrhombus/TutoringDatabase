7d8d2ee99ba022dcfcc95152783dd102a86621fe
Tom Perry <tomperry7@gmail.com>
2022-03-24 21:48:24 -0600

    database functionality works with test script

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 import Student as st
 
-persona = st.Student("Tom")
+#persona = st.Student("Tom")
+#persona.addparent("Bob")
+#persona.addparent("Kathy")
+#persona.changeaddress("123 disneyland street")
+##print("\n")
+##print( persona.name )
+##print( persona.parents )
+##print( persona.address )
+#
+#info = persona.storeinfo()
+#print(info)
+#
+#personb = st.Student("Tomddd")
+#personb.addparent("Bobddd")
+#personb.addparent("Kathyddd")
+#personb.changeaddress("ddd 123 disneyland street")
 
-print( persona.name )
-print( persona.parents )
-print( persona.address )
+theDB = st.StudentDB("./studentdatabase.db")
+#theDB.set(persona.storeinfo())
+#theDB.set(persona.storeinfo())
+#theDB.set(personb.storeinfo())
+me = theDB.getstudent("Tom")
+print(me.name)
+print(me.address)
+print(me.parents)
 
-persona.addparent("Bob")
-persona.changeaddress("123 disneyland street")
-
-print("\n")
-print( persona.name )
-print( persona.parents )
-print( persona.address )
